afa7f4401bdcd820c0dd8dc051fa49ba8a909b9b
Jeff Trojan <j.dauda@alustudent.com>
2023-02-19 02:30:50 +0200

    Restful

---
+++ restful-practice/index.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const
+  port = 8888;
+const express = require('express');
+const app = express();
+
+//  /hello/ GET request
+app.get('/hello/:name?', (req, res) => {
+  res.json(
+    {
+      message: `Hello ${req.params.name || 'world'}!`
+    }
+  );
+});
+
+// start server
+
+app.listen(port, () =>
+  console.table(`Server started on port ${port}`)
+);
